bef3e2b40cc1837f85aa6e91a5351895fe903009
Sefat Anam <sefatanam@gmail.com>
2020-04-13 15:34:25 +0600

    form building of add customer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { StoreModule } from '@ngrx/store';
 import { AppRoutingModule } from './app-routing.module';
 import { CustomersModule } from './customers/customers.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NavbarComponent],
@@ -16,7 +17,7 @@ import { CustomersModule } from './customers/customers.module';
     BrowserAnimationsModule,
     StoreModule.forRoot({}),
     AppRoutingModule,
-    CustomersModule
+    CustomersModule,FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/customers/customer-add/customer-add.component.ts
+++ src/app/customers/customer-add/customer-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Customer } from '../customer.model';
 
 @Component({
   selector: 'app-customer-add',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-add.component.css']
 })
 export class CustomerAddComponent implements OnInit {
+  customerForm : FormGroup;
 
-  constructor() { }
+  constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+
+  
+
+  private validators(){
+    this.customerForm = this.fb.group({
+      name: ["", Validators.required],
+      phone: ["", Validators.required],
+      address: ["", Validators.required],
+      membership: ["", Validators.required]
+    });
+  }
+
+  createCustomer() {
+    const newCustomer: Customer = {
+      name: this.customerForm.get("name").value,
+      phone: this.customerForm.get("phone").value,
+      address: this.customerForm.get("address").value,
+      membership: this.customerForm.get("membership").value
+    };
+  }
+
 
 }
